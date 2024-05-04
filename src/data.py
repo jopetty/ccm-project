543ae628b2f5696efc472968a7950ca38daca7cd
jopetty <research@jacksonpetty.org>
2024-05-03 23:09:44 -0400

    fix training

--- src/data.py
+++ src/data.py
@@ -29,6 +29,7 @@ class SpecialTokens(StrEnum):
     EOS = "[EOS]"
     SEP = "[SEP]"
     CLS = "[CLS]"
+    PAD = "[PAD]"
     MASK = "[MASK]"
 
     @classmethod
@@ -66,12 +67,14 @@ class OSFArgs:
         self.update = True
 
 
-def tokenize(examples: DatasetDict, tokenizer: PreTrainedTokenizerFast) -> DatasetDict:
+def tokenize(
+    examples: DatasetDict, tokenizer: PreTrainedTokenizerFast, max_length: int
+) -> DatasetDict:
     """Tokenize dataset."""
     return tokenizer(
         examples["text"],
         padding="max_length",
-        max_length=512,
+        max_length=max_length,
         truncation=True,
         return_tensors="pt",
     )
@@ -125,7 +128,7 @@ def get_chars(example: Dataset) -> set:
     return codepoints
 
 
-def load_data(large_track: bool, subsample: int | None) -> dict:
+def load_data(large_track: bool, subsample: int | None, seed: int) -> dict:
     """Load BabyLM data into HF dataset object."""
     if large_track:
         data_files = {
@@ -143,11 +146,11 @@ def load_data(large_track: bool, subsample: int | None) -> dict:
     dataset = load_dataset("text", data_files=data_files)
 
     if subsample is not None:
-        dataset["train"] = dataset["train"].select(range(subsample))
-        dataset["validation"] = dataset["validation"].select(range(subsample))
-        dataset["test"] = dataset["test"].select(range(subsample))
-
-    print(dataset)
+        dataset["train"] = dataset["train"].shuffle(seed=seed).select(range(subsample))
+        dataset["validation"] = (
+            dataset["validation"].shuffle(seed=seed).select(range(subsample))
+        )
+        dataset["test"] = dataset["test"].shuffle(seed=seed).select(range(subsample))
 
     num_workers = 8
     pool = Pool(processes=num_workers)
@@ -166,15 +169,20 @@ def load_data(large_track: bool, subsample: int | None) -> dict:
 def get_initial_tokenizer(unique_tokens: set[str]):
     """Get char-level tokenizer."""
     tokenizer_base = Tokenizer(WordLevel())
+    # tokenizer_base.normalizer = normalizers.Sequence([
+    #     NFD(),
+    #     StripAccents(),
+    #     Lowercase()
+    # ])
+
     tokenizer_base.pre_tokenizer = WhitespaceSplit()
     tokenizer_base.add_special_tokens(SpecialTokens.values())
     tokenizer_base.add_tokens(list(unique_tokens))
 
     tokenizer_base.post_processor = TemplateProcessing(
-        single=f"{SpecialTokens.BOS} $A {SpecialTokens.EOS}",
+        single=f"{SpecialTokens.BOS} $A",
         special_tokens=[
             (SpecialTokens.BOS, tokenizer_base.token_to_id(SpecialTokens.BOS)),
-            (SpecialTokens.EOS, tokenizer_base.token_to_id(SpecialTokens.EOS)),
         ],
     )
 
@@ -251,29 +259,35 @@ def merge_new_tokens(
 
 
 def construct_dataset(
-    seed: int, block_size: int, large_track: bool, subsample: int | None
+    seed: int,
+    block_size: int,
+    large_track: bool,
+    subsample: int | None,
+    tokenizer: PreTrainedTokenizerFast | None,
 ):
     """Construct BabyLM dataset and initial tokenizer."""
     # Check if PROJECT_ROOT / data has more than a single .gitkeep file in it
     if not len(list(PROJECT_ROOT.glob("data/*"))) > 1:
         download_data()
 
-    data = load_data(large_track=large_track, subsample=subsample)
+    data = load_data(large_track=large_track, subsample=subsample, seed=seed)
     dataset = data["dataset"]
     all_chars = data["all_chars"]
 
-    tokenizer = get_initial_tokenizer(all_chars)
+    if tokenizer is None:
+        tokenizer = get_initial_tokenizer(all_chars)
+    else:
+        tokenizer.add_tokens(list(all_chars))
 
-    tokenize_map = partial(tokenize, tokenizer=tokenizer)
+    tokenize_map = partial(tokenize, tokenizer=tokenizer, max_length=block_size)
     dataset = dataset.map(
         tokenize_map,
-        batched=True,
         num_proc=os.cpu_count(),
         remove_columns=dataset["train"].column_names,
     )
 
-    # Shuffle before stacking
-    dataset["train"] = dataset["train"].shuffle(seed=seed)
+    # # Shuffle before stacking
+    # dataset["train"] = dataset["train"].shuffle(seed=seed)
 
     stack_map = partial(stack_sequences, block_size=block_size)
     dataset = dataset.map(
